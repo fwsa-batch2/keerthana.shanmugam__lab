fc66d86d07039d2b4ab09fda99c4d09092b6bc0d
keerthana-shanmugam <keerthanas089@gmail.com>
2022-04-26 15:11:30 +0530

    new exercise

---
+++ JAVA/src/com/Fwsa/encapsulation.java
@@ -0,0 +1,37 @@
+package com.Fwsa;
+
+class UserData{
+	private String email;
+	private int password;
+	
+	public String getemail() {
+	    return email;
+	}
+	
+	public void setemail(String email){
+		this.email = email;
+	}
+	
+	public int getpassword() {
+		return password;
+	}
+	
+	public void setpassword(int password) {
+		 this.password = password;
+	}
+}
+
+public class encapsulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      UserData myObj = new UserData();
+      
+      myObj.setemail("keerthana@mail.com");
+      myObj.setpassword(45678);
+      
+      System.out.println("email : "+ myObj.getemail());
+	}
+
+}
+
