4bf6febb4d59ab4cce490a068533a61a76a5d4c7
keerthana-shanmugam <keerthanas089@gmail.com>
2022-06-16 15:27:55 +0530

    ruby problems

--- Ruby/HasesTest.rb
+++ Ruby/HasesTest.rb
@@ -4,9 +4,8 @@ library = {
     
     "lahiri" => "Interpreter of Maladies"
 }
-  
-  puts library["lahiri"]
-  
+puts library["lahiri"]
+
   # Practice : 2
 library = {}
 library["austen"] = ["Pride and Prejudice", "Sense and Sensibility"]
@@ -16,5 +15,48 @@ authors = library.keys
 puts "These are the authors in my catalog : #{authors.join(', ')}"
 
 library.each do |author, books|
-  puts "#{author.capitalize} wrote the books #{books.join(', ')}"
+puts "#{author.capitalize} wrote the books #{books.join(', ')}"
 end
+
+   # problem : 1
+books = ["Design as Art", "Anathem", "Shogun"]
+authors = ["Bruno Munari", "Neal Stephenson", "James Clavell"]
+
+cat = {}
+for value in 0...books.length do
+    for a in 0...authors.length do
+        if value == a
+            k = authors[a].split.first.downcase.to_sym
+            r = 
+            books[value]
+            cat.store(k,r)
+        end
+    end
+end
+p cat
+
+  # problem : 2
+todos = [
+    ["Send invoice", "money"],
+    ["Clean room", "organize"],
+    ["Pay rent", "money"],
+    ["Arrange books", "organize"],
+    ["Pay taxes", "money"],
+    ["Buy groceries", "food"]
+  ]
+
+  arr1 = []
+  arr2 = []
+  arr3 = []
+  dog = {}
+
+for val in 0...todos.length do
+for tiger in 0...todos[val].length do
+    if todos[val][tiger + 1] == "money"
+       array1 = arr1.push(todos[val][tiger])
+        dog.store((todos[val][tiger+1]).to_sym,array1)
+    end
+    
+end
+end
+p dog
---
+++ Ruby/functionTest.rb
@@ -0,0 +1,10 @@
+   # Functions problem : 
+def salute(one,two)
+    fun1 = one.split.last.capitalize
+    fun2 = two.capitalize
+  
+    puts "#{fun2} Mr. #{fun1}"
+end
+
+salute("Nelson Rolihlahla Mandela", "hello")
+salute("Sir Alan Turing", "welcome")
