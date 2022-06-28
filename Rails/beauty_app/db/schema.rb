3c16b3e1745ed28bc1bf0839441c9c36de7c9cc7
keerthana-shanmugam <keerthanas089@gmail.com>
2022-06-28 18:55:33 +0530

    todo practice

--- Rails/beauty_app/db/schema.rb
+++ Rails/beauty_app/db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_24_101142) do
+ActiveRecord::Schema.define(version: 2022_06_28_100412) do
 
   create_table "todos", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "task"
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "status", default: false
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
